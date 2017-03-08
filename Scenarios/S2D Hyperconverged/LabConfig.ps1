e5119be46ce9969aaeec3ed9be199a936413207a
Jaromir Kaspar <jaromirk@microsoft.com>
2017-03-08 15:41:44 +0100

    structure change

---
+++ Scenarios/S2D Hyperconverged/LabConfig.ps1
@@ -0,0 +1,42 @@
+﻿##### Labconfig.ps1 #####
+$LabConfig=@{
+    DomainAdminName='LabAdmin'; 			
+    AdminPassword='LS1setup!'; 			
+    Prefix = 'S2DHyperConverged-'; 		
+    SwitchName = 'LabSwitch';			
+    DCEdition='ServerDataCenter';		
+    VMs=@()								
+} 
+
+1..4 | % { 
+	$VMNames="S2D"; 							
+	$LABConfig.VMs += @{ 
+		VMName = "$VMNames$_" ; 
+		Configuration = 'S2D' ; 				
+		ParentVHD = 'Win2016NanoHV_G2.vhdx';	
+		SSDNumber = 0; 							
+		SSDSize=800GB ; 						
+		HDDNumber = 12; 						
+		HDDSize= 4TB ; 							
+		MemoryStartupBytes= 512MB 				
+	} 
+} 
+
+#### Or with Nested Virtualization. ####
+<#
+1..4 | % { 
+	$VMNames="S2D"; 							
+	$LABConfig.VMs += @{ 
+		VMName = "$VMNames$_" ; 
+		Configuration = 'S2D' ; 				
+		ParentVHD = 'Win2016NanoHV_G2.vhdx';	
+		SSDNumber = 0; 							
+		SSDSize=800GB ; 						
+		HDDNumber = 12; 						
+		HDDSize= 4TB ; 							
+		MemoryStartupBytes= 4GB;
+        NestedVirt=$True 				
+	} 
+}
+
+#>
