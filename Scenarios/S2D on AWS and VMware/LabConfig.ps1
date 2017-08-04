6038da46c18e8654b8e6f6a4e29ac59086ba12d0
Jaromir Kaspar <jaromirk@microsoft.com>
2017-08-04 10:02:22 +0200

    aws/vmware scenario

---
+++ Scenarios/S2D on AWS and VMware/LabConfig.ps1
@@ -0,0 +1,2 @@
+$LabConfig=@{ DomainAdminName='LabAdmin'; AdminPassword='LS1setup!'; Prefix = 'ws2016lab-'; SwitchName = 'LabSwitch'; DCEdition='DataCenter'; AdditionalNetworksConfig=@(); VMs=@(); ServerVHDs=@()}
+1..2 | % {$VMNames="S2D"; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; ParentVHD = 'Win2016Core_G2.vhdx'; SSDNumber = 0; SSDSize=800GB ; HDDNumber = 12; HDDSize= 4TB ; MemoryStartupBytes= 512MB }} 
