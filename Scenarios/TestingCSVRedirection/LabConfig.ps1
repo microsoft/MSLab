73bcc686edb39cdd1bb34e1cc4e8e6a78b47f2b5
Jaromir Kaspar <jaromirk@microsoft.com>
2017-08-07 22:43:16 +0200

    Testing CSV redirection

---
+++ Scenarios/TestingCSVRedirection/LabConfig.ps1
@@ -0,0 +1,5 @@
+$LabConfig=@{ DomainAdminName='LabAdmin'; AdminPassword='LS1setup!'; Prefix = 'ws2016lab-'; SwitchName = 'LabSwitch'; DCEdition='DataCenter'; AdditionalNetworksConfig=@(); VMs=@(); ServerVHDs=@()}
+
+1..2 | % { $VMNames="SAN"       ; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'Shared' ; ParentVHD = 'Win2016NanoHV_G2.vhdx' ; SSDNumber = 1; SSDSize=1GB   ; HDDNumber = 4 ; HDDSize= 4TB ; MemoryStartupBytes= 4GB ; VMSet= 'SAN'      ; NestedVirt=$True} }
+1..2 | % { $VMNames="SharedSS"  ; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'Shared' ; ParentVHD = 'Win2016NanoHV_G2.vhdx' ; SSDNumber = 3; SSDSize=800GB ; HDDNumber = 9 ; HDDSize= 4TB ; MemoryStartupBytes= 4GB ; VMSet= 'SharedSS' ; NestedVirt=$True} }
+1..2 | % { $VMNames="S2D"       ; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D'    ; ParentVHD = 'Win2016NanoHV_G2.vhdx' ; SSDNumber = 0; SSDSize=800GB ; HDDNumber = 4 ; HDDSize= 4TB ; MemoryStartupBytes= 4GB ;                     NestedVirt=$True} }
