653cc3369ec56754d74cf0d373a0e98bd59bd8aa
Jaromir Kaspar <jaromirk@microsoft.com>
2017-10-05 01:32:25 +0200

    fix

---
+++ 1709/readme.md
@@ -0,0 +1,12 @@
+# Server 1709 lab
+
+## Howto
+To create Windows Server 1709 lab, just replace LabConfig.ps1 with the 1709 labconfig from [this](https://github.com/Microsoft/ws2016lab/tree/master/1709/LabConfig.ps1) page.
+
+Download Windows Server 1709 bits [here](https://www.microsoft.com/en-us/evalcenter/)
+
+Hydrate labs as [usual](https://github.com/Microsoft/ws2016lab#howto)
+
+Continue with [Hyper-V with SAN scenario](https://github.com/Microsoft/ws2016lab/tree/master/Scenarios/Hyper-V%201709%20with%20SAN) (add servers in labconfig as per scenario labconfig)
+
+Give a try to [Project Honolulu](http://aka.ms/honoluludownload). Just download honolulu bits, copy over to Management machine and install.
--- Insider/LabConfig.ps1
+++ Insider/LabConfig.ps1
@@ -1,9 +1,9 @@
 $LabConfig=@{ DomainAdminName='LabAdmin'; AdminPassword='LS1setup!'; Prefix = 'ws2016labInsider-'; SwitchName = 'LabSwitch'; DCEdition='SERVERDATACENTERACORE'; CreateClientParent=$True ; ClientEdition='Enterprise' ; PullServerDC=$false; Internet=$false ; AdditionalNetworksConfig=@(); VMs=@(); ServerVHDs=@()}
 $LabConfig.VMs += @{ VMName = 'Management' ; Configuration = 'Simple' ; ParentVHD = 'Win10_G2.vhdx'  ; MemoryStartupBytes= 1GB ; AddToolsVHD=$True ; DisableWCF=$True }
-1..4 | % {$VMNames="S2D"; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; ParentVHD = 'WinServer1709_G2.vhdx'; SSDNumber = 0; SSDSize=800GB ; HDDNumber = 12; HDDSize= 4TB ; MemoryStartupBytes= 512MB }}
+1..4 | % {$VMNames="S2D"; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; ParentVHD = 'WinServerInsider_G2.vhdx'; SSDNumber = 0; SSDSize=800GB ; HDDNumber = 12; HDDSize= 4TB ; MemoryStartupBytes= 512MB }}
 
 $LABConfig.ServerVHDs += @{
     Edition="SERVERDATACENTERACORE";
-    VHDName="WinServer1709_G2.vhdx";
+    VHDName="WinServerInsider_G2.vhdx";
     Size=30GB
 }
