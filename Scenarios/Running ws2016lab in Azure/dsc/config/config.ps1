37f0608c6d4f6b821b9bce3225b01ae19302fcc8
Jaromir Kaspar <jaromirk@microsoft.com>
2018-01-12 10:49:04 +0100

    fix
    Former-commit-id: 997161a27b45934d77fa2db7d4c1c7da854c8063

--- Scenarios/Running ws2016lab in Azure/dsc/config/config.ps1
+++ Scenarios/Running ws2016lab in Azure/dsc/config/config.ps1
@@ -9,19 +9,22 @@ Configuration Config {
         }
         Script CreateFolder
         {
-            SetScript = "New-Item -Type Directory -Name ws2016lab -Path d:"
-            TestScript = "Test-Path -Path d:\ws2016lab"
-            GetScript = "@{Ensure = if (Test-Path -Path d:\ws2016lab) {'Present'} else {'Absent'}}"
+            SetScript = {New-Item -Type Directory -Name ws2016lab -Path d:}
+            TestScript = {Test-Path -Path d:\ws2016lab}
+            GetScript = {   @{Ensure = if (Test-Path -Path d:\ws2016lab) {'Present'} else {'Absent'}}   }
+            DependsOn = "[WindowsFeature]HyperV"
         }
         Script DownloadScripts
         {
-            SetScript = "Invoke-WebRequest -UseBasicParsing -Uri https://github.com/Microsoft/ws2016lab/blob/master/scripts.zip?raw=true -OutFile d:\scripts.zip"
-            TestScript = "Test-Path -Path d:\scripts.zip"
-            GetScript = "@{Ensure = if (Test-Path -Path d:\scripts.zip) {'Present'} else {'Absent'}}"
+            SetScript = {Invoke-WebRequest -UseBasicParsing -Uri https://github.com/Microsoft/ws2016lab/blob/master/scripts.zip?raw=true -OutFile d:\scripts.zip}
+            TestScript = {Test-Path -Path d:\scripts.zip}
+            GetScript = { @{Ensure = if (Test-Path -Path d:\scripts.zip) {'Present'} else {'Absent'}} }
         }
         Script UnzipScripts
         {
-            SetScript = "Expand-Archive d:\scripts.zip -DestinationPath d:\ws2016lab"
+            SetScript = {Expand-Archive d:\scripts.zip -DestinationPath d:\ws2016lab -Force}
+            TestScript = {!("1_Prereq.ps1","2_CreateParentDisks.ps1","3_Deploy.ps1","Cleanup.ps1","LabConfig.ps1" | ForEach-Object {Test-Path -Path d:\ws2016lab\$_}).contains($false)}
+            GetScript = {   @{Ensure = if (!("1_Prereq.ps1","2_CreateParentDisks.ps1","3_Deploy.ps1","Cleanup.ps1","LabConfig.ps1" | ForEach-Object {Test-Path -Path d:\ws2016lab\$_}).contains($false)) {'Present'} else {'Absent'}} }
             DependsOn = "[Script]DownloadScripts"
         }
         
