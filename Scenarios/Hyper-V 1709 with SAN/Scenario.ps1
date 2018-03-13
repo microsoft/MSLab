ae360366018e18dcdf025892efa69a7bef2feb04
Jaromir Kaspar <kaspik@hotmail.com>
2018-03-13 15:33:40 +0100

    fix

--- Scenarios/Hyper-V 1709 with SAN/Scenario.ps1
+++ Scenarios/Hyper-V 1709 with SAN/Scenario.ps1
@@ -214,7 +214,9 @@ Write-host "Script started at $StartDateTime"
             Invoke-Command -ComputerName $servers -ScriptBlock {Set-NetQosDcbxSetting -willing $false -confirm:$false}
 
         #Configure IeeePriorityTag
-            Invoke-Command -ComputerName $servers -ScriptBlock {Set-VMNetworkAdapter -ManagementOS -Name "SMB*" -IeeePriorityTag on}
+            #It is not really needed. IeePriorityTag needs to be On (if you want tag your SMB, nonRDMA traffic for QoS) only for adapters that pass vSwitch (both SR-IOV and RDMA bypasses vSwitch)
+            #Invoke-Command -ComputerName $servers -ScriptBlock {Set-VMNetworkAdapter -ManagementOS -Name "SMB*" -IeeePriorityTag on}
+
         #validate flow control setting
             Invoke-Command -ComputerName $servers -ScriptBlock { Get-NetQosFlowControl} | Sort-Object  -Property PSComputername | ft PSComputerName,Priority,Enabled -GroupBy PSComputerNa
 
