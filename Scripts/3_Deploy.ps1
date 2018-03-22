84391479bba831592ba05d583f6100893d5fb8e6
AndreasSobczyk <andreas.sobczyk@gmail.com>
2018-03-22 22:07:17 +0100

    dd support for configurable vCPU on multi-socket +  global variable

--- Scripts/3_Deploy.ps1
+++ Scripts/3_Deploy.ps1
@@ -647,7 +647,7 @@ If (!( $isAdmin )) {
     $WindowsInstallationType=Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\' -Name InstallationType
 
     #Grab number of processors
-    (get-wmiobject win32_processor).NumberOfLogicalProcessors  | ForEach-Object { $NumberOfLogicalProcessors += $_}
+    (get-wmiobject win32_processor).NumberOfLogicalProcessors  | ForEach-Object { $global:NumberOfLogicalProcessors += $_}
 
 #endregion
 
