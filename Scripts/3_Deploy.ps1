d8b46b1fcfdae2aeecf142ad7d675b727e131849
Jaromir Kaspar <jaromirk@microsoft.com>
2018-04-24 22:12:12 +0200

    fix
    Former-commit-id: ea603598fba9a8556042cf0943b7c954eeef70a2

--- Scripts/3_Deploy.ps1
+++ Scripts/3_Deploy.ps1
@@ -201,7 +201,6 @@ If (!( $isAdmin )) {
         <RegisteredOwner>PFE</RegisteredOwner>
         <RegisteredOrganization>PFE Inc.</RegisteredOrganization>
     </component>
-    </component>
     <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         <RunSynchronous>
             $RunSynchronous
