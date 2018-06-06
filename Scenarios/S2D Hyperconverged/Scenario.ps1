11732d2a3f818c8717222370def0c27bd58b15ec
Jaromir Kaspar <jaromirk@microsoft.com>
2018-06-06 13:11:36 +0200

    fix

--- Scenarios/S2D Hyperconverged/Scenario.ps1
+++ Scenarios/S2D Hyperconverged/Scenario.ps1
@@ -566,6 +566,11 @@ Write-host "Script started at $StartDateTime"
             if ($processor.NumberOfCores -eq $processor.NumberOfLogicalProcessors/2){
                 $HT=$True
             }
+            #Calculate max processors
+            $number=[math]::log($processor.NumberOfCores-1) / [math]::log( 2 )
+            $number=[math]::Truncate($number)
+            $Maxprocessors=[math]::pow( 2, $number )
+
             $adapters=@()
             $switch.NetAdapterInterfaceDescriptions | ForEach-Object {$adapters+=Get-NetAdapterHardwareInfo -InterfaceDescription $_ -CimSession $switch.computername}
 
@@ -575,8 +580,8 @@ Write-host "Script started at $StartDateTime"
                 }else{
                     $BaseProcessorNumber=$adapter.NumaNode*$processor.NumberOfLogicalProcessors+1
                 }
-                $adapter=Get-NetAdapter -InterfaceDescription $adapter.InterfaceDescription -CimSession $adapter.PSComputerName  
-                $adapter | Set-NetAdapterVmq -BaseProcessorNumber $BaseProcessorNumber -MaxProcessors ($processor.NumberOfCores-1)
+                $adapter=Get-NetAdapter -InterfaceDescription $adapter.InterfaceDescription -CimSession $adapter.PSComputerName
+                $adapter | Set-NetAdapterVmq -BaseProcessorNumber $BaseProcessorNumber -MaxProcessors $Maxprocessors
                 $adapter | Set-NetAdapterRss -Profile Closest
             }
         }
