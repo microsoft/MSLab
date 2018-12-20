1beaabfcb3d650585ee36af31f874147bb35bd3f
Jaromir Kaspar <jaromirk@microsoft.com>
2018-12-20 16:09:03 +0100

    added Validate-DCB
    Former-commit-id: d92c17f7efdc429eb7ac6a3404ae3be10ecfaa57

--- Scenarios/S2D and Validate-DCB/Config.ps1
+++ Scenarios/S2D and Validate-DCB/Config.ps1
@@ -13,8 +13,8 @@ $Nodes | ForEach-Object {
                 EmbeddedTeamingEnabled = $true
 
                 RDMAEnabledAdapters = @(
-                    @{ Name = 'Ethernet'   ; VMNetworkAdapter = 'SMB_1' ; VLANID = '1' ; JumboPacket = 1500 }
-                    @{ Name = 'Ethernet 1' ; VMNetworkAdapter = 'SMB_2' ; VLANID = '1' ; JumboPacket = 1500 }
+                    @{ Name = 'Ethernet'   ; VMNetworkAdapter = 'SMB_1' ; VLANID = '1' ; JumboPacket = 1514 }
+                    @{ Name = 'Ethernet 2' ; VMNetworkAdapter = 'SMB_2' ; VLANID = '1' ; JumboPacket = 1514 }
                 )
 
                 RDMADisabledAdapters = @(
@@ -29,7 +29,7 @@ $NonNodeData = @{
     NetQoS = @(
         @{ Name = 'ClusterHB'; Template = 'Cluster'              ; PriorityValue8021Action = 5 ; BandwidthPercentage = 1  ; Algorithm = 'ETS' }
         @{ Name = 'SMB'      ; NetDirectPortMatchCondition = 445 ; PriorityValue8021Action = 3 ; BandwidthPercentage = 60 ; Algorithm = 'ETS' }
-        @{ Name = 'DEFAULT'  ; Template = 'Default'              ; PriorityValue8021Action = 0 ; BandwidthPercentage = 39 ; Algorithm = 'ETS' }
+        @{ Name = 'Default'  ; Template = 'Default'              ; PriorityValue8021Action = 0 ; BandwidthPercentage = 39 ; Algorithm = 'ETS' }
     )
 }
 
