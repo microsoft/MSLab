08f10000bf0d416c6e6769e5496cb4e6bf3a4e6d
Jaromir Kaspar <jaromirk@microsoft.com>
2019-03-21 11:19:14 +0100

    Metadata scenario update

--- Scripts/2_CreateParentDisks.ps1
+++ Scripts/2_CreateParentDisks.ps1
@@ -324,12 +324,14 @@ If (!( $isAdmin )) {
             VHDName="Win2016Core_G2.vhdx"
             Size=30GB
         }
+        <# Removed since it does not work with newer than 14393.2724
         $ServerVHDs += @{
             Edition="DataCenterNano"
             VHDName="Win2016NanoHV_G2.vhdx"
             NanoPackages="Microsoft-NanoServer-DSC-Package","Microsoft-NanoServer-FailoverCluster-Package","Microsoft-NanoServer-Guest-Package","Microsoft-NanoServer-Storage-Package","Microsoft-NanoServer-SCVMM-Package","Microsoft-NanoServer-Compute-Package","Microsoft-NanoServer-SCVMM-Compute-Package","Microsoft-NanoServer-SecureStartup-Package","Microsoft-NanoServer-DCB-Package","Microsoft-NanoServer-ShieldedVM-Package"
             Size=30GB
         }
+        #>
     }elseif ($BuildNumber -eq 17763){
         #Windows Server 2019
         $ServerVHDs += @{
