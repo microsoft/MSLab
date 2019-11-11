4bcab7dc3fda947da39fb25d8ef48393408b6a1f
Jaromir Kaspar <jaromirk@microsoft.com>
2019-11-11 16:36:12 +0100

    fix

--- Scenarios/S2D and Cloud Services Onboarding/scenario.ps1
+++ Scenarios/S2D and Cloud Services Onboarding/scenario.ps1
@@ -36,8 +36,9 @@ Start-Process -Wait -Filepath msiexec.exe -Argumentlist "/i $env:UserProfile\Dow
 If ((Get-ExecutionPolicy) -ne "RemoteSigned"){Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force}
 Login-AzAccount -UseDeviceAuthentication
 #select context if more available
-if ((Get-AzContext -ListAvailable).count -gt 1){
-    get-azcontent -ListAvailable | Out-GridView -OutpuMode Single | Set-AzContent
+$context=Get-AzContext -ListAvailable
+if (($context).count -gt 1){
+    $context | Out-GridView -OutpuMode Single | Set-AzContext
 }
 
 #Grab Insights Workspace
@@ -100,6 +101,103 @@ Copy-Item -Path "$env:USERPROFILE\Downloads\OMSGateway.msi" -Destination "$env:U
 #https://docs.microsoft.com/en-us/azure/azure-monitor/platform/gateway#install-the-log-analytics-gateway-using-the-command-line
 Invoke-Command -ComputerName $LAGatewayName -ScriptBlock {
     Start-Process -FilePath msiexec.exe -ArgumentList "/I $env:USERPROFILE\Downloads\OMSGateway.msi /qn LicenseAccepted=1" -Wait
+}
+#endregion
+
+#region deploy a Windows Hybrid Runbook Worker
+#https://docs.microsoft.com/en-us/azure/automation/automation-windows-hrw-install
+
+#Workspace/Resource Group Name (same as Log Analytics)
+$SubscriptionID=(Get-AzContext).Subscription.ID
+$WorkspaceName="WSLabWorkspace-$SubscriptionID"
+$ResourceGroupName="WSLabWinAnalytics"
+$HRWorkerServerName="HRWorker01"
+$AutomationAccountName="WSLabAutomationAccount"
+$HybridWorkerGroupName="WSLabHRGroup01"
+
+#Add the Automation, AlertManagement, Updates and security solution to the Log Analytics workspace
+#Get-AzOperationalInsightsIntelligencePack -ResourceGroupName $ResourceGroupName -WorkspaceName $WorkspaceName
+$solutions="AzureAutomation","AlertManagement","Updates","Security"
+foreach ($solution in $solutions){
+    Set-AzOperationalInsightsIntelligencePack -ResourceGroupName $ResourceGroupName -WorkspaceName $WorkspaceName -IntelligencePackName $solution -Enabled $true
+}
+
+#Add Automation Account
+$location=(Get-AzOperationalInsightsWorkspace -Name $WorkspaceName -ResourceGroupName $ResourceGroupName).Location
+New-AzAutomationAccount -Name $AutomationAccountName -ResourceGroupName $ResourceGroupName -Location $Location -Plan Free 
+
+#link workspace to Automation Account <tbd>
+<#
+$json=@"
+{
+    "type": "Microsoft.OperationalInsights/workspaces",
+    "name": "[variables('namespace')]",
+    "apiVersion": "2017-03-15-preview",
+    "location": "[resourceGroup().location]",
+    "properties": {
+        "sku": {
+            "name": "Standalone"
+        }
+    },
+    "resources": [
+        {
+            "name": "Automation", # this onboards automation to oms, which is what you need
+            "type": "linkedServices",
+            "apiVersion": "2015-11-01-preview",
+            "dependsOn": [
+                "[variables('automation')]",
+                "[variables('namespace')]"
+            ],
+            "properties": {
+                "resourceId": "[resourceId('Microsoft.Automation/automationAccounts/', variables('automation'))]"
+            }
+        }
+    ]
+}
+"@
+$TemplateObject = ConvertFrom-Json $json -AsHashtable
+New-AzDeployment -Location $Location -TemplateObject $json
+#>
+
+#Install MMA to Hybrid Runbook worker server
+#Download MMA Agent (if not yet downloaded)
+if (-not (Test-Path "$env:USERPROFILE\Downloads\MMASetup-AMD64.exe")){
+    $ProgressPreference='SilentlyContinue' #for faster download
+    Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?LinkId=828603 -OutFile "$env:USERPROFILE\Downloads\MMASetup-AMD64.exe" -UseBasicParsing
+    $ProgressPreference='Continue' #return progress preference back
+}
+
+#Increase MaxEvenlope and create session to copy files to
+Invoke-Command -ComputerName $HRWorkerServerName -ScriptBlock {Set-Item -Path WSMan:\localhost\MaxEnvelopeSizekb -Value 4096}
+$session=New-PSSession -ComputerName $HRWorkerServerName
+
+#copy mma agent
+Copy-Item -Path "$env:USERPROFILE\Downloads\MMASetup-AMD64.exe" -Destination "$env:USERPROFILE\Downloads\" -tosession $session -force
+
+#grab WorkspaceID
+$WorkspaceID=$Workspace.CustomerId.guid
+#Grab WorkspacePrimaryKey
+$WorkspacePrimaryKey=($Workspace | Get-AzOperationalInsightsWorkspaceSharedKey).PrimarySharedKey
+
+#install MMA agent
+Invoke-Command -ComputerName $HRWorkerServerName -ScriptBlock {
+    $ExtractFolder="$env:USERPROFILE\Downloads\MMAInstaller"
+    #extract MMA
+    if (Test-Path $extractFolder) {
+        Remove-Item $extractFolder -Force -Recurse
+    }
+    Start-Process -FilePath "$env:USERPROFILE\Downloads\MMASetup-AMD64.exe" -ArgumentList "/c /t:$ExtractFolder" -Wait
+    Start-Process -FilePath "$ExtractFolder\Setup.exe" -ArgumentList "/qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=0 OPINSIGHTS_WORKSPACE_ID=$using:workspaceId OPINSIGHTS_WORKSPACE_KEY=$using:workspacePrimaryKey AcceptEndUserLicenseAgreement=1" -Wait
+}
+
+#Register the hybrid runbook worker
+$AutomationInfo = Get-AzAutomationRegistrationInfo -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName
+$AutomationPrimaryKey = $AutomationInfo.PrimaryKey
+$AutomationEndpoint = $AutomationInfo.Endpoint
+Invoke-Command -ComputerName $HRWorkerServerName -ScriptBlock {
+    $PoshModule=(get-childitem -Path "$env:programfiles\Microsoft Monitoring Agent\Agent\AzureAutomation" -Recurse  | Where-Object name -eq HybridRegistration.psd1 | Sort-Object LastWriteTime -Descending | Select-Object -First 1).FullName
+    Import-Module $PoshModule
+    Add-HybridRunbookWorker -Name $using:HybridWorkerGroupName -EndPoint $using:AutomationEndpoint -Token $using:AutomationPrimaryKey
 }
 #endregion
 
