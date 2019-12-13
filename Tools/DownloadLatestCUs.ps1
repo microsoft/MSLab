6715d2307e7295c9f29b507c456b81a6acd25424
Jaromir Kaspar <jaromirk@microsoft.com>
2019-12-13 12:21:22 +0100

    added tool to download CUs+SSUs

---
+++ Tools/DownloadLatestCUs.ps1
@@ -0,0 +1,44 @@
+If ((Get-ExecutionPolicy) -ne "RemoteSigned"){
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+}
+
+#try import module
+Import-Module LatestUpdate -ErrorAction Ignore
+
+#download LatestUpdate module
+if (!(get-module -Name LatestUpdate)){
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    Install-Module -Name LatestUpdate -Force
+}
+
+#grab folder to download to
+$folder=Read-Host -Prompt "Please type path to download. For example `"c:\temp\`" (if nothing specified, $PSScriptRoot is used)"
+if(!$folder){$folder=$PSScriptRoot}
+
+#grab versions to download
+$versions="1607","1703","1709","1803","1809","1903" | Out-GridView -OutputMode Multiple -Title "Select versions to download"
+
+foreach ($Version in $versions){
+    Write-Output "Fetching info from web"
+    $latestCU=Get-LatestCumulativeUpdate -OperatingSystem WindowsServer -Version $Version | Select-Object -First 1 #1903 has 2 objects
+    $latestSSU=Get-LatestServicingStackUpdate -OperatingSystem Windows10 -Version $Version | Where-Object -Property Note -Like "*server*" 
+    $CUFilename=$latestCU.URL.Split('/') | Select-Object -Last 1
+    $SSUFilename=$latestSSU.URL.Split('/') | Select-Object -Last 1
+    $path="$folder$version\$($latestCU.Note.Substring(0,7))"
+
+    if (Test-Path -Path "$path\$CUFilename"){
+        Write-Output "$CUFilename already present. Skipping download"
+    }else{
+        Write-Output "Downloading $CUFilename"
+        $latestCU | Save-LatestUpdate -Path $path
+    }
+    if (Test-Path -Path "$path\$SSUFilename"){
+        Write-Output "$SSUFilename already present. Skipping download"
+    }else{
+        Write-Output "Downloading $SSUFilename"
+        $latestSSU | Save-LatestUpdate -Path $path
+    }
+}
+
+Write-Host "Job finished. Press enter to continue" -ForegroundColor Green
+Read-Host
