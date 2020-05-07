28cb9b3ddc2cfdcec98c7a73480d9e4e7cb033e5
Jaromir Kaspar <jaromirk@microsoft.com>
2020-05-07 14:41:12 +0200

    udated download links for latest versions
    Former-commit-id: 0f75c14121252cfea68d8fc53de366e489c1b304

--- Scenarios/Securing LDAP/Scenario.ps1
+++ Scenarios/Securing LDAP/Scenario.ps1
@@ -298,7 +298,7 @@ foreach ($TemplateName in $TemplateNames){
 
 #region Download and Install Edge
 $ProgressPreference='SilentlyContinue' #for faster download
-Invoke-WebRequest -Uri "http://dl.delivery.mp.microsoft.com/filestreamingservice/files/07367ab9-ceee-4409-a22f-c50d77a8ae06/MicrosoftEdgeEnterpriseX64.msi" -UseBasicParsing -OutFile "$env:USERPROFILE\Downloads\MicrosoftEdgeEnterpriseX64.msi"
+Invoke-WebRequest -Uri "http://dl.delivery.mp.microsoft.com/filestreamingservice/files/40e309b4-5d46-4AE8-b839-bd74b4cff36e/MicrosoftEdgeEnterpriseX64.msi" -UseBasicParsing -OutFile "$env:USERPROFILE\Downloads\MicrosoftEdgeEnterpriseX64.msi"
 $ProgressPreference='Continue' #to set it back
 #start install
 Start-Process -Wait -Filepath msiexec.exe -Argumentlist "/i $env:UserProfile\Downloads\MicrosoftEdgeEnterpriseX64.msi /q"
