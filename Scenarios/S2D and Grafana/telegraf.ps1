6c4a1d3d33f3e411c19e603e332b1ce928d4a70f
paracetamol32 <dominique_manas@hotmail.com>
2020-05-12 21:18:50 +0200

    Update telegraf.ps1
    ## Module need to be installed with command "Install-Module influx"
    1. Removed Send-JsonOverTcp function
    2. Replaced with influx module
    3. No more need multiple listener ports on infuxdb
    3. Add metrics from get-clusterperf

--- Scenarios/S2D and Grafana/telegraf.ps1
+++ Scenarios/S2D and Grafana/telegraf.ps1
@@ -1,67 +1,75 @@
-﻿Function Send-JsonOverTcp {
-    param ( [ValidateNotNullOrEmpty()] 
-    [string] $Ip, 
-    [int] $Port, 
-    $JsonObject) 
-    $JsonString = $JsonObject -replace "`n",' ' -replace "`r",' ' -replace ' ',''
-    $Socket = New-Object System.Net.Sockets.TCPClient($Ip,$Port) 
-    $Stream = $Socket.GetStream() 
-    $Writer = New-Object System.IO.StreamWriter($Stream)
-    $Writer.WriteLine($JsonString)
-    $Writer.Flush()
-    $Stream.Close()
-    $Socket.Close()
-}
-
+$URLInfluxDB ="http://127.0.0.1:8086"
 $csvs = Get-ClusterSharedVolume
 
-#Get CSV Info
-    foreach ($csv in $csvs ){
-        $csvinfos = $csv | Select-Object -Property Name -ExpandProperty SharedVolumeInfo
-        foreach ( $csvinfo in $csvinfos ){
-            $data = $csv.Name
-            $name = ($data.split("(")[1]).split(")")[0]
-            $obj = New-Object PSObject -Property @{
-                FriendlyName        = $name
-                Path        = $csvinfo.FriendlyVolumeName
-                Size        = $csvinfo.Partition.Size
-                FreeSpace   = $csvinfo.Partition.FreeSpace
-                UsedSpace   = $csvinfo.Partition.UsedSpace
-                PercentFree = $csvinfo.Partition.PercentFree
-            }
+foreach ( $csv in $csvs ) {
+    $csvinfos = $csv | Select-Object -Property Name -ExpandProperty SharedVolumeInfo
+    foreach ( $csvinfo in $csvinfos ) {
+        $data = $csv.Name
+        $name = ($data.split("(")[1]).split(")")[0]
+        $obj = New-Object PSObject -Property @{
+            FriendlyName = $name
+            Path         = $csvinfo.FriendlyVolumeName
+            Size         = $csvinfo.Partition.Size
+            FreeSpace    = $csvinfo.Partition.FreeSpace
+            UsedSpace    = $csvinfo.Partition.UsedSpace
+            PercentFree  = $csvinfo.Partition.PercentFree
         }
-       $csvinfo = $obj |Select-Object FriendlyName,Path,@{ Label = "Size(GB)" ; Expression = { ($_.Size/1024/1024/1024) } },@{ Label = "FreeSpace(GB)" ; Expression = { ($_.FreeSpace/1024/1024/1024) } },@{ Label = "UsedSpace(GB)" ; Expression = {  ($_.UsedSpace/1024/1024/1024) } },@{ Label = "PercentFree" ; Expression = {  ($_.PercentFree) }} | ConvertTo-Json
-       Send-JsonOverTcp 127.0.0.1 8094 "$csvinfo"
     }
+    $csvinfo = $obj | Select-Object FriendlyName, Path, @{ Label = "Size" ; Expression = { ($_.Size / 1024 / 1024 / 1024) } }, @{ Label = "FreeSpace" ; Expression = { ($_.FreeSpace / 1024 / 1024 / 1024) } }, @{ Label = "UsedSpace" ; Expression = { ($_.UsedSpace / 1024 / 1024 / 1024) } }, @{ Label = "PercentFree" ; Expression = { ($_.PercentFree) } } 
+    $csvinfo | ConvertTo-Metric -Measure CSVinfo -MetricProperty Size, FreeSpace, UsedSpace, PercentFree -TagProperty FriendlyName, Path | Write-Influx -Database HC  -Server $URLInfluxDB
+}
  
-
 $VDisks = Get-VirtualDisk 
-
-foreach ( $Vdisk in $VDisks ){
-    # Convert HealtStatus To Num
-    If ( $VDisk.HealthStatus -like "Healthy"){
+foreach ( $Vdisk in $VDisks ) {
+    If ( $VDisk.HealthStatus -like "Healthy") {
         $HeatlyNum = 1
-    } elseif ( $VDisk.HealthStatus -like "Warning") {
+    }
+    elseif ( $VDisk.HealthStatus -like "Warning") {
         $HeatlyNum = 2
     }
-    $VDiskinfo = Get-VirtualDisk -FriendlyName $Vdisk.FriendlyName | Select-Object FriendlyName,ResiliencySettingName,OperationalStatus,HealthStatus,@{ Label = "HealthNum" ; Expression = {  ($HeatlyNum) } },@{ Label = "Size(GB)" ; Expression = {  ($_.Size/1024/1024/1024) } },@{ Label = "AllocatedSize(GB)" ; Expression = {  ($_.AllocatedSize/1024/1024/1024) } },@{ Label = "StorageEfficiency" ; Expression = {  (($_.Size*100)/$_.FootprintOnPool) } },@{ Label = "FootprintOnPool (GB)" ; Expression = {  ($_.FootprintOnPool/1024/1024/1024) } } | ConvertTo-Json
-
-    Send-JsonOverTcp 127.0.0.1 8094 "$VDiskinfo"
-    #Write-Host $Vdisk
+    $VDiskinfo = Get-VirtualDisk -FriendlyName $Vdisk.FriendlyName | Select-Object FriendlyName, ResiliencySettingName, OperationalStatus, HealthStatus, @{ Label = "HealthNum" ; Expression = { ($HeatlyNum) } }, @{ Label = "Size" ; Expression = { ($_.Size / 1024 / 1024 / 1024) } }, @{ Label = "AllocatedSize" ; Expression = { ($_.AllocatedSize / 1024 / 1024 / 1024) } }, @{ Label = "StorageEfficiency" ; Expression = { (($_.Size * 100) / $_.FootprintOnPool) } }, @{ Label = "FootprintOnPool" ; Expression = { ($_.FootprintOnPool / 1024 / 1024 / 1024) } } 
+    $VDiskinfo | ConvertTo-Metric -Measure VDiskinfo -MetricProperty HealthNum, Size, AllocatedSize, StorageEfficiency, FootprintOnPool -TagProperty FriendlyName, ResiliencySettingName, OperationalStatus, HealthStatus | Write-Influx -Database HC  -Server $URLInfluxDB
 }
 
-$Jobs =  get-storagejob | Select-Object name,JobState,PercentComplete | ConvertTo-Json
+Get-storagejob | ConvertTo-Metric -Measure StorageJob -TagProperty name, JobState -MetricProperty PercentComplete | Write-Influx -Database HC  -Server $URLInfluxDB
+Get-WmiObject -Class MSCluster_Node -namespace "root\mscluster" | ConvertTo-Metric -Measure ClusterState -TagProperty name  -MetricProperty id,state | Write-Influx -Database HC  -Server $URLInfluxDB
+Get-StorageQoSVolume | Select-Object @{ Label = "Mountpoint" ; Expression = { (($_.Mountpoint.split("\\"))[2]) } }, IOPS, Latency, Bandwidth | ConvertTo-Metric -Measure StorageQoSVolume -TagProperty Mountpoint -MetricProperty IOPS, Latency, Bandwidth | Write-Influx -Database HC  -Server $URLInfluxDB
+Get-StorageQosFlow | Select-Object InitiatorName, @{Expression = { $_.InitiatorNodeName.Substring(0, $_.InitiatorNodeName.IndexOf('.')) }; Label = "InitiatorNodeName" }, StorageNodeIOPs | ConvertTo-Metric -Measure StorageQoSVM -TagProperty InitiatorName,InitiatorNodeName -MetricProperty StorageNodeIOPs | Write-Influx -Database HC  -Server $URLInfluxDB
 
-Send-JsonOverTcp 127.0.0.1 8095 "$Jobs"
+$FirstNode = (Get-ClusterNode | Where-Object State -Match 'UP').name | Select-Object -First 1
 
-$statusclust = get-clusternode | Select-Object name,state| ConvertTo-Json
-
-Send-JsonOverTcp 127.0.0.1 8096 "$statusclust"
-
-$qos = Get-StorageQoSVolume | Select-Object @{ Label = "Mountpoint" ; Expression = { (($_.Mountpoint.split("\\"))[2])}},IOPS,Latency,Bandwidth | ConvertTo-Json
-
-Send-JsonOverTcp 127.0.0.1 8097 "$qos"
-
-$qosvm = Get-StorageQosFlow |Select-Object InitiatorName, @{Expression={$_.InitiatorNodeName.Substring(0,$_.InitiatorNodeName.IndexOf('.'))};Label="InitiatorNodeName"},StorageNodeIOPs | ConvertTo-Json
-
-Send-JsonOverTcp 127.0.0.1 8098 "$qosvm"
+if ($FirstNode -match "$env:computername") {
+    if (((get-date).second) -in 0..10) {
+        $VMtable = get-vm | Get-ClusterPerf | Select-Object ObjectDescription, MetricId, Value
+        $i = 1
+        $hash = $null
+        $hash = @{ }
+        foreach ($values in $VMtable) {  
+            $vmname = $values.ObjectDescription.split([char]0x0020, 1) | Select-Object -last 1
+            $metric = $values.MetricId.split([char]0x002C, 1) | Select-Object -first 1
+            $currentvalue = [math]::Round(($values.Value), 0)  
+            If ($i -le 18) { $hash.add($metric, $currentvalue) }
+            $i++
+            If ($i -eq 19) {
+                $i = 1
+                #Write-InfluxUDP -IP 192.168.132.20 -Port 8889 -Measure VM -Tags @{VMName = $vmname } -Metrics $hash 
+                Write-Influx -Database HC  -Server $URLInfluxDB -Measure VM -Metrics $hash -Tags @{VMName = $vmname }
+                $hash = $null
+                $hash = @{ }
+            }
+        }
+    }
+    ## S2D Metrics
+    if (((get-date).second) -in 30..40) {
+        $S2DPerfs = Get-ClusterPerf | Select-Object MetricID, value
+        $hash = $null
+        $hash = @{ }
+        $hash.add("host", $env:computername)
+        foreach ($S2DPerf in $S2DPerfs) {     
+            $metric = $S2DPerf.MetricId.split([char]0x002C, 1) | Select-Object -first 1
+            $currentvalue = $S2DPerf.Value
+            $hash.add($metric, $currentvalue)
+        }
+        Write-Influx -Database HC  -Server $URLInfluxDB -Measure S2D -Metrics $hash -Tags @{host = $env:computername}
+    }
+}
