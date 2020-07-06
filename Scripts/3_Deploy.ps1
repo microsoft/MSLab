fbc7e51cd731d1eba77609be524d26d4a4f02155
Jaromir Kaspar <jaromirk@microsoft.com>
2020-07-06 11:07:20 +0200

    added OEM info to unattend (WSLab)

--- Scripts/3_Deploy.ps1
+++ Scripts/3_Deploy.ps1
@@ -93,6 +93,10 @@ If (-not $isAdmin) {
   </settings>
   <settings pass="specialize">
     <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+      <OEMInformation>
+        <SupportProvider>WSLab</SupportProvider>
+        <SupportURL>https://aka.ms/wslab</SupportURL>
+      </OEMInformation>
       <RegisteredOwner>PFE</RegisteredOwner>
       <RegisteredOrganization>PFE Inc.</RegisteredOrganization>
     </component>
@@ -141,8 +145,12 @@ If (-not $isAdmin) {
  <settings pass="specialize">
     <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         <ComputerName>$Computername</ComputerName>
+        <OEMInformation>
+          <SupportProvider>WSLab</SupportProvider>
+          <SupportURL>https://aka.ms/wslab</SupportURL>
+        </OEMInformation>
         <RegisteredOwner>PFE</RegisteredOwner>
-          <RegisteredOrganization>PFE Inc.</RegisteredOrganization>
+        <RegisteredOrganization>PFE Inc.</RegisteredOrganization>
     </component>
     <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         <RunSynchronous>
@@ -205,6 +213,10 @@ If (-not $isAdmin) {
  <settings pass="specialize">
     <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         <ComputerName>$Computername</ComputerName>
+        <OEMInformation>
+          <SupportProvider>WSLab</SupportProvider>
+          <SupportURL>https://aka.ms/wslab</SupportURL>
+        </OEMInformation>
         <RegisteredOwner>PFE</RegisteredOwner>
         <RegisteredOrganization>PFE Inc.</RegisteredOrganization>
     </component>
