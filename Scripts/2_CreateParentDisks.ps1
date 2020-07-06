a20681256d87e455e2259468878ad18159cf4630
Jaromir Kaspar <jaromirk@microsoft.com>
2020-07-06 11:07:20 +0200

    added OEM info to unattend (WSLab)
    Former-commit-id: fbc7e51cd731d1eba77609be524d26d4a4f02155

--- Scripts/2_CreateParentDisks.ps1
+++ Scripts/2_CreateParentDisks.ps1
@@ -81,6 +81,10 @@ If (-not $isAdmin) {
  <settings pass="specialize">
     <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         <ComputerName>$Computername</ComputerName>
+        <OEMInformation>
+          <SupportProvider>WSLab</SupportProvider>
+          <SupportURL>https://aka.ms/wslab</SupportURL>
+        </OEMInformation>
         <RegisteredOwner>PFE</RegisteredOwner>
         <RegisteredOrganization>Contoso</RegisteredOrganization>
     </component>
