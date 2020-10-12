47b9cabcac03894f6be5f622c3b38e5df1c937bf
Jaromir Kaspar <jaromirk@microsoft.com>
2020-10-12 22:51:50 +0200

    fixes

--- Scenarios/AzSHCI and MDT/Scenario.ps1
+++ Scenarios/AzSHCI and MDT/Scenario.ps1
@@ -1,22 +1,23 @@
 #run all from DC
 
+#region download and install binaries
+
 #download folder location
 $downloadfolder="$env:USERPROFILE\Downloads"
 
 #Download files
 $ProgressPreference="SilentlyContinue"
 $files=@()
-$Files+=@{Uri="https://go.microsoft.com/fwlink/?linkid=2026036" ; FileName="adksetup.exe"}
-$Files+=@{Uri="https://go.microsoft.com/fwlink/?linkid=2022233" ; FileName="adkwinpesetup.exe"}
-$Files+=@{Uri="https://download.microsoft.com/download/3/3/9/339BE62D-B4B8-4956-B58D-73C4685FC492/MicrosoftDeploymentToolkit_x64.msi" ; FileName="MicrosoftDeploymentToolkit_x64.msi"}
-$Files+=@{Uri="https://go.microsoft.com/fwlink/?linkid=866658" ; FileName="SQL2019-SSEI-Expr.exe"}
-$Files+=@{Uri="https://software-download.microsoft.com/download/pr/AzureStackHCI_17784.1068_EN-US.iso" ; FileName="AzureStackHCI_17784.1068_EN-US.iso"}
-#$Files+=@{Uri="https://aka.ms/ssmsfullsetup" ; FileName="SSMS-Setup-ENU"} #SQL Management Studio
-
+$Files+=@{Uri="https://go.microsoft.com/fwlink/?linkid=2026036" ; FileName="adksetup.exe" ; Description="Windows 10 ADK 1809"}
+$Files+=@{Uri="https://go.microsoft.com/fwlink/?linkid=2022233" ; FileName="adkwinpesetup.exe" ; Description="WindowsPE 1809"}
+$Files+=@{Uri="https://download.microsoft.com/download/3/3/9/339BE62D-B4B8-4956-B58D-73C4685FC492/MicrosoftDeploymentToolkit_x64.msi" ; FileName="MicrosoftDeploymentToolkit_x64.msi" ; Description="Microsoft Deployment Toolkit"}
+$Files+=@{Uri="https://go.microsoft.com/fwlink/?linkid=866658" ; FileName="SQL2019-SSEI-Expr.exe" ; Description="Microsoft Deployment Toolkit"}
+$Files+=@{Uri="https://software-download.microsoft.com/download/pr/AzureStackHCI_17784.1068_EN-US.iso" ; FileName="AzureStackHCI_17784.1068_EN-US.iso" ; Description="Azure Stack HCI ISO"}
+#$Files+=@{Uri="https://aka.ms/ssmsfullsetup" ; FileName="SSMS-Setup-ENU" ; Description="SQL Management Studio"}
 
 foreach ($file in $files){
     if (-not (Test-Path "$downloadfolder\$($file.filename)")){
-        Write-Output "Downloading $($file.filename)"
+        Write-Output "Downloading $($file.Description)"
         Invoke-WebRequest -UseBasicParsing -Uri $file.uri -OutFile "$downloadfolder\$($file.filename)"
     }
 }
@@ -32,6 +33,10 @@ $exec="$downloadfolder\SQL2019-SSEI-Expr.exe"
 $params="/Action=Install /MediaPath=$downloadfolder\SQLMedia /IAcceptSqlServerLicenseTerms /quiet"
 Start-Process -FilePath $exec -ArgumentList $params -NoNewWindow -Wait
 
+#endregion
+
+#region configure MDT
+
 #import MDT Module
 Import-Module "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
 #list commands
@@ -40,10 +45,11 @@ Get-Command -Module MicrosoftDeploymentToolkit
 #Create new Deployment Share
 New-Item -Path c:\ -Name DeploymentShare -ItemType Directory
 New-SmbShare -Name "DeploymentShare$" -Path "C:\DeploymentShare" -FullAccess Administrators
-Import-Module "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
+
+#map MDT deployment share as PSDrive
 New-PSDrive -Name "DS001" -PSProvider "MDTProvider" -Root "C:\DeploymentShare" -Description "MDT Deployment Share" -NetworkPath "\\DC\DeploymentShare$" -Verbose | add-MDTPersistentDrive -Verbose
 
-#Enable Named Pipes for sql
+#Enable Named Pipes for SQLServer
 Set-ItemProperty -Path "hklm:\\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQLServer\SuperSocketNetLib\Np\" -Name Enabled -Value 1
 #Set-ItemProperty -Path "hklm:\\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQLServer\SuperSocketNetLib\Tcp\" -Name Enabled -Value 1
 #Set-ItemProperty -Path "hklm:\\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQLServer\SuperSocketNetLib\Tcp\IPAll" -Name TcpPort -Value 1433
@@ -70,10 +76,13 @@ Set-ItemProperty DS001:\ -name Boot.x64.IncludeNetworkDrivers -value True
 Set-ItemProperty DS001:\ -name Boot.x64.IncludeMassStorageDrivers -value True
 Set-ItemProperty DS001:\ -name Boot.x64.IncludeAllDrivers -value False
 Set-ItemProperty DS001:\ -name Boot.x64.GenerateGenericWIM -value True
-Set-ItemProperty DS001:\ -name MonitorHost -value DC
 
 #add Task Sequence
 import-mdttasksequence -path "DS001:\Task Sequences" -Name "Azure Stack HCI Deploy" -Template "Server.xml" -Comments "" -ID "AzSHCI" -Version "1.0" -OperatingSystemPath "DS001:\Operating Systems\Azure Stack HCI SERVERAZURESTACKHCICORE in Azure Stack HCI SERVERAZURESTACKHCICORE x64 install.wim" -FullName "PFE" -OrgName "Contoso" -HomePage "about:blank" -AdminPassword "LS1setup!" -Verbose
+
+#endregion
+
+#region configure MDT run-as account
 
 #create identity for MDT
 New-ADUser -Name MDTUser -AccountPassword  (ConvertTo-SecureString "LS1setup!" -AsPlainText -Force) -Enabled $True -Path  "ou=workshop,dc=corp,dc=contoso,dc=com"
@@ -81,6 +90,9 @@ New-ADUser -Name MDTUser -AccountPassword  (ConvertTo-SecureString "LS1setup!" -
 #add FileShare permissions for MDT Account
 Grant-SmbShareAccess -Name DeploymentShare$ -AccessRight Read -AccountName MDTUser -Confirm:$false
 
+#endregion
+
+#region configure Bootstrap ini and generate WinPE
 #populate bootstrap.ini
 $content=@'
 [Settings]
@@ -98,6 +110,10 @@ $content | Set-Content -Path C:\DeploymentShare\Control\Bootstrap.ini
 #update deployment share
 update-mdtdeploymentshare -path "DS001:" -verbose -force
 
+#endregion
+
+#region Install and configure WDS
+
 #install WDS
 Install-WindowsFeature WDS -IncludeManagementTools -IncludeAllSubFeature
 wdsutil /initialize-server /reminst:"C:\RemoteInstall"
@@ -107,15 +123,43 @@ wdsutil.exe /Set-Server /AnswerClients:Known
 WDSUTIL /Set-Server /PxePromptPolicy /known:Noprompt /new:abort
 #WDSUTIL /Set-Server /PxePromptPolicy /known:Noprompt /new:Noprompt
 
-#import the boot media to WDS 
+#import the boot media to WDS
 Get-WdsBootImage |Remove-WdsBootImage
 Import-wdsbootimage -path C:\DeploymentShare\Boot\LiteTouchPE_x64.wim -Verbose
 
-#########################
-# Run from Hyper-V Host #
-#########################
+#endregion
 
-#region Run from Hyper-V Host to create new VMs
+#region configure MDT Monitoring
+if (-not(Get-PSDrive -Name ds001)){
+    New-PSDrive -Name "DS001" -PSProvider "MDTProvider" -Root "C:\DeploymentShare" -Description "MDT Deployment Share" -NetworkPath "\\DC\DeploymentShare$" -Verbose | add-MDTPersistentDrive -Verbose
+}
+#configure ports in MDT
+Set-ItemProperty DS001:\ -name MonitorHost -value DC
+#enable service
+Enable-MDTMonitorService -EventPort 9800 -DataPort 9801
+#add firewall rule
+New-NetFirewallRule `
+-Action Allow `
+-Name "MDT-Monitoring-In-TCP" `
+-DisplayName "MDT (Monitoring-In-TCP)" `
+-Description "Inbound rule for MDT Monitoring. [TCP-9800,9801]" `
+-Enabled True `
+-Direction Inbound `
+-Program "System" `
+-Protocol UDP `
+-LocalPort 9800,9801 `
+-Profile Any `
+-Group "MDT" `
+-RemoteAddress Any
+#endregion
+
+#####################################
+#       Run from Hyper-V Host       #
+#                                   #
+# ! Adjust LabPrefix and VMs Path ! #
+#####################################
+
+#region Run from Hyper-V Host to create new, empty VMs
     #some variables
     $LabPrefix="WSLab17763.1339-"
     $vSwitchName="$($LabPrefix)LabSwitch"
@@ -155,6 +199,17 @@ Import-wdsbootimage -path C:\DeploymentShare\Boot\LiteTouchPE_x64.wim -Verbose
 # Run from DC #
 ###############
 
+#region Create hash table out of machines that attempted boot last 5 minutes
+
+#in real world scenairos you can have hash table like this:
+<#
+$HVHosts = @()
+$HVHosts+=@{ComputerName="AzSHCI1"  ;IPAddress="10.0.0.101" ; MACAddress="00:15:5D:01:20:3B" ; GUID="FFB429BB-1521-47C6-88BF-F5F2D1BA17F5"}
+$HVHosts+=@{ComputerName="AzSHCI2"  ;IPAddress="10.0.0.102" ; MACAddress="00:15:5D:01:20:3E" ; GUID="056423D7-6BAC-460F-AD0D-78AF6D26E151"}
+$HVHosts+=@{ComputerName="AzSHCI3"  ;IPAddress="10.0.0.103" ; MACAddress="00:15:5D:01:20:40" ; GUID="D1DF2157-8EE0-4C7A-A0FD-6E7779C62FCE"}
+$HVHosts+=@{ComputerName="AzSHCI4"  ;IPAddress="10.0.0.104" ; MACAddress="00:15:5D:01:20:42" ; GUID="7392BBB5-99D7-4EEE-9C22-4CA6EB6058FB"}
+#>
+
 #grab machines that attempted to boot and create hash table of 
 $IpaddressScope="10.0.0."
 $IPAddressStart=101 #starting this number IPs will be asigned
@@ -174,7 +229,10 @@ foreach ($event in $events){
     }
 }
 
-#add deploy info to AD Object and MDT Database
+#endregion
+
+#region add deploy info to AD Object and MDT Database
+
 #download and unzip mdtdb
 Invoke-WebRequest -Uri https://msdnshared.blob.core.windows.net/media/TNBlogsFS/prod.evol.blogs.technet.com/telligent.evolution.components.attachments/01/5209/00/00/03/24/15/04/MDTDB.zip -OutFile $env:USERPROFILE\Downloads\MDTDB.zip
 Expand-Archive -Path $env:USERPROFILE\Downloads\MDTDB.zip -DestinationPath $env:USERPROFILE\Downloads\MDTDB\
@@ -220,14 +278,15 @@ if (-not (Get-MDTRole -name JoinDomain)){
     }
 }
 
-#allow machines to boot from PXE from DC
+#allow machines to boot from PXE from DC by adding info into AD Object
 foreach ($HVHost in $HVHosts){
     [guid]$guid=$HVHost.GUID
     Set-ADComputer -identity $hvhost.ComputerName -replace @{netbootGUID = $guid}
     #Set-ADComputer -identity $hvhost.ComputerName -replace @{netbootMachineFilePath = "DC"}
 }
+#endregion
 
-#replace customsettings.ini with all DB data to query (wizard output)
+#region replace customsettings.ini with all DB data to query (wizard output)
 $content=@'
 [Settings]
 Priority=CSettings, CPackages, CApps, CAdmins, CRoles, Locations, LSettings, LPackages, LApps, LAdmins, LRoles, MMSettings, MMPackages, MMApps, MMAdmins, MMRoles, RSettings, RPackages, RApps, RAdmins, Default
@@ -238,6 +297,7 @@ OSInstall=Y
 SkipCapture=YES
 SkipAdminPassword=NO
 SkipProductKey=YES
+EventService=http://DC:9800
 
 [CSettings]
 SQLServer=dc
@@ -438,6 +498,10 @@ Priority=CSettings, CPackages, CApps, CAdmins, CRoles, Locations, LSettings, LPa
 '@
 $content | Set-Content C:\DeploymentShare\Control\CustomSettings.ini
 
+#endregion
+
+#region configure SQL to be able to access it remotely using MDTUser account
+
 #create Firewall rule for SQL Server, so client can access settings
 <#
 New-NetFirewallRule `
@@ -486,7 +550,9 @@ GO
 ALTER ROLE [db_datareader] ADD MEMBER [corp\mdtuser]
 GO
 '@
-Invoke-Sqlcmd -ServerInstance dc\sqlexpress -Database MDTDB -Query $sqlscriptcc
+Invoke-Sqlcmd -ServerInstance dc\sqlexpress -Database MDTDB -Query $sqlscript
+
+#endregion
 
 ################################################
 # restart hyper-v machines to let them install #
