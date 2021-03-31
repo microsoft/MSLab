cee31a19bf3e769ef0e96158f81557bc5e4ab786
Jaromir Kaspar <jaromirk@microsoft.com>
2021-03-31 16:38:32 +0200

    New logic in script

--- Tools/DownloadLatestCUs.ps1
+++ Tools/DownloadLatestCUs.ps1
@@ -2,12 +2,111 @@ If ((Get-ExecutionPolicy) -ne "RemoteSigned"){
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
 }
 
-#set TLS 1.2 for github downloads
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#region variables
 
-#download LatestUpdate module
-Write-Output "Checking if LatestUpdate PS Module is Installed"
-if (!(Get-InstalledModule -Name LatestUpdate -ErrorAction Ignore)){
+#grab folder to download to
+$folder=Read-Host -Prompt "Please type path to download. For example `"c:\temp`" (if nothing specified, $PSScriptRoot is used)"
+if(!$folder){$folder=$PSScriptRoot}
+
+#do you want preview?
+$preview=Read-Host -Prompt "Do you want to download preview updates? Y/N, default N"
+if($preview -eq "y"){$
+    preview = $true
+}else{
+    $preview=$false
+}
+
+#URLs with list of latest updates
+$URLs=@()
+$URLs+=@{Product="Windows10";URL="https://support.microsoft.com/en-us/topic/release-notes-for-azure-stack-hci-64c79b7f-d536-015d-b8dd-575f01090efd"}
+$URLs+=@{Product="AzureStackHCI";URL="https://support.microsoft.com/en-us/topic/windows-10-update-history-7dd3071a-3906-fa2c-c342-f7f86728a6e3"}
+
+$UpdatesList=@()
+$Titles=@()
+#endregion
+
+#region process
+#download content
+Write-Output "Downloading content of latest update sites"
+foreach ($url in $urls){
+    $content=Invoke-WebRequest -Uri $url.url
+    $UpdatesList+=($content.ParsedHtml.getElementsByClassName("supLeftNavArticle") | Select-Object OuterText).OuterText
+    $Titles+=$content.ParsedHtml.getElementsByClassName("supLeftNavCategoryTitle") | Select-Object textcontent
+}
+
+#clean white space
+$CleanedList=@()
+Foreach ($item in $UpdatesList){
+    $CleanedList+=$item.Trim()
+}
+
+#remove initial win10
+$Titles=$Titles | Where-Object {$_.textcontent -notlike "*initial version*"}
+
+#clean white space
+$CleanedListTitles=@()
+Foreach ($Title in $Titles){
+    $CleanedListTitles+=$Title.TextContent.Trim()
+}
+
+#Process data
+Write-Output "Processing data"
+$i=0
+$Output=@()
+foreach ($item in $CleanedList){
+    if ($item -match $CleanedListTitles[$i]){
+        $Title=$CleanedListTitles[$i]
+        $i++
+    }else{
+        $DateString=([Regex]::Match($item,'\w*\ \d*,\ \d*')).Value
+        if (($DateString -split " ")[1].Length -eq 3){
+            $format="MMMM dd, yyyy"
+        }else{
+            $format="MMMM d, yyyy"
+        }
+        $Date=([datetime]::ParseExact($DateString,$format,$null))
+
+        if ($item -like "*Preview*"){
+            $ReleaseType="Preview"
+        }elseif ($item -like "*Out-Of-Band*"){
+            $ReleaseType="Out-Of-Band"
+        }elseif ($item -like "*servicing*"){
+            $ReleaseType="ServicingStackUpdate"
+        }else{
+            $ReleaseType="Standard"
+        }
+        $Product=$Title.Replace(", version","").Replace(" update history","").Replace(", and"," and")
+        #$Product="$(([Regex]::Match($Title,'^.+?(?=,)')).Value)$(([Regex]::Match($Title,'\ and\ .+?(?=,)')).value) $(([Regex]::Match($Title,"(?<=version\ )\w{4}")).Value)"
+        $Output += [PSCustomObject]@{
+            "Product"=$Product
+            "Version"=([Regex]::Match($Title,"(?<=version\ )\w{4}")).Value
+            "NavCategoryTitle"=$Title
+            "Date"= $Date
+            "KB" = ([Regex]::Match($item,'KB\d*')).Value
+            "Build" = ([Regex]::Match($item,'\d*\.\d*')).Value
+            "ReleaseType" = $ReleaseType
+        }
+    }
+}
+
+
+$Products=$Output | Select-Object Product -Unique | Out-GridView -Title "Please select Products you want to download latest updates for" -OutputMode Multiple
+
+$Result=@()
+Foreach ($Product in $products){
+    if ($preview -eq $true){
+        $result+=$Output | Where-Object {$_.Product -eq $Product.Product -and $_.ReleaseType -notlike "ServicingStackUpdate"} | Select-Object -First 1
+        $result+=$Output | Where-Object {$_.Product -eq $Product.Product -and $_.ReleaseType -eq "ServicingStackUpdate"} | Select-Object -First 1
+    }else{
+        $result+=$Output | Where-Object {$_.Product -eq $Product.Product -and $_.ReleaseType -eq "Standard"} | Select-Object -First 1
+        $result+=$Output | Where-Object {$_.Product -eq $Product.Product -and $_.ReleaseType -eq "ServicingStackUpdate"} | Select-Object -First 1
+    }
+}
+#endregion
+
+#region download MSCatalog module
+Write-Output "Checking if MSCatalog PS Module is Installed"
+if (!(Get-InstalledModule -Name MSCatalog -ErrorAction Ignore)){
     # Verify Running as Admin
     $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
     If (!( $isAdmin )) {
@@ -16,57 +115,33 @@ if (!(Get-InstalledModule -Name LatestUpdate -ErrorAction Ignore)){
         exit
     }
     Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-    Install-Module -Name LatestUpdate -Force
+    Install-Module -Name MSCatalog -Force
 }
 
-#check if latest json is added
-$jsonweb=(Invoke-WebRequest -UseBasicParsing -Uri https://raw.githubusercontent.com/aaronparker/LatestUpdate/master/LatestUpdate/LatestUpdate.json).content | ConvertFrom-Json 
-$jsonlocal=Get-Content -Path "$((Get-InstalledModule -Name LatestUpdate).InstalledLocation)\LatestUpdate.json" | ConvertFrom-Json
-if (($jsonlocal.ParameterValues.Windows10Versions).Count -ne ($jsonweb.ParameterValues.Windows10Versions).Count){
-    Write-Output "JSON on Local web is different, replacing local JSON"
-    #elevate process
-    $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
-    If (!( $isAdmin )) {
-        Write-Host "-- Restarting as Administrator to replace JSON in LatestUpdate Module" -ForegroundColor Cyan ; Sleep -Seconds 1
-        Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs 
-        exit
-    }
-    $jsonweb | ConvertTo-Json | Out-File "$((Get-InstalledModule -Name LatestUpdate).InstalledLocation)\LatestUpdate.json"
-    If ((Get-ExecutionPolicy) -ne "RemoteSigned"){
-        Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
-    }
-}
+#endregion
 
-#grab folder to download to
-$folder=Read-Host -Prompt "Please type path to download. For example `"c:\temp`" (if nothing specified, $PSScriptRoot is used)"
-if(!$folder){$folder=$PSScriptRoot}
-
-#grab versions to download
-$versions=$jsonweb.ParameterValues.Windows10Versions | Out-GridView -OutputMode Multiple -Title "Select versions to download"
-
-foreach ($Version in $versions){
-    Write-Output "Fetching for latest CU info from web"
-    $latestCU=Get-LatestCumulativeUpdate -OperatingSystem Windows10 -Version $Version | Where-Object -Property Note -Like "*Windows 10*x64*" | Select-Object -First 1 #1903 has 2 objects
-    $CUFilename=$latestCU.URL.Split('/') | Select-Object -Last 1
-    $path="$folder\$version\$($latestCU.Note.Substring(0,7))"
-    New-Item -Path $path -ItemType Directory -ErrorAction Ignore | Out-Null
-    if (Test-Path -Path "$path\$CUFilename"){
-        Write-Output "$CUFilename already present. Skipping download"
-    }else{
-        Write-Output "Downloading $CUFilename"
-        $latestCU | Save-LatestUpdate -Path $path
-    }
-
-    Write-Output "Fetching for latest SSU info from web"
-    $latestSSU=Get-LatestServicingStackUpdate -OperatingSystem Windows10 -Version $Version | Where-Object -Property Note -Like "*Windows 10*x64*"
-    $SSUFilename=$latestSSU.URL.Split('/') | Select-Object -Last 1
-    if (Test-Path -Path "$path\$SSUFilename"){
-        Write-Output "$SSUFilename already present. Skipping download"
-    }else{
-        Write-Output "Downloading $SSUFilename"
-        $latestSSU | Save-LatestUpdate -Path $path
+#region Download update
+Write-Output "Downloading Updates"
+Foreach ($item in $Result){
+    $DateFolderName=($result | where Product -eq $item.Product | where ReleaseType -ne ServicingStackUpdate).Date | Get-Date -Format "dd-MMM-yy"
+    $Path="C:\temp\$($item.Product)\$DateFolderName"
+    New-Item -Path $Path -ItemType Directory -ErrorAction Ignore
+    $UpdateCandidate=Get-MSCatalogUpdate -Search $item.kb | Where-Object Title -Like "*x64*"
+    if ($UpdateCandidate.count -eq 1){
+        $UpdateCandidate | Save-MSCatalogUpdate -UseBits -Destination $Path
+    }elseif ($item.Product -like "Azure Stack*"){
+        $UpdateCandidate | Where-Object Products -Like "*Azure Stack*" | Save-MSCatalogUpdate -UseBits -Destination $Path
+    }elseif ($item.Product -like "*Windows Server*"){
+        $update=$UpdateCandidate | Where Title -Like "*Windows Server*"
+        if ($update.count -eq 1){
+            $update | Save-MSCatalogUpdate -UseBits -Destination $Path
+        }else{
+            $update | Where-Object Title -Like "*$($item.Version)*" | Save-MSCatalogUpdate
+        }
     }
 }
+
+#endregion
 
 Write-Host "Job finished. Press enter to continue" -ForegroundColor Green
 Read-Host
