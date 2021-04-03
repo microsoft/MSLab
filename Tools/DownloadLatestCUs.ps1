dac76443d231c48c0a8f8e45330f3ecdf07aa3eb
Jaromir Kaspar <jaromirk@microsoft.com>
2021-04-03 23:28:54 +0200

    simplified a bit

--- Tools/DownloadLatestCUs.ps1
+++ Tools/DownloadLatestCUs.ps1
@@ -1,10 +1,10 @@
 $Products=@()
-$Products+=@{Product="Azure Stack HCI 20H2";SearchString="Cumulative Update for Azure Stack HCI, version 20H2"                ;PreviewSearchString="Cumulative Update Preview for Azure Stack HCI"                              ;SSUSearchString="Servicing Stack Update for Azure Stack HCI, version 20H2 for x64-based Systems"}
-$Products+=@{Product="Windows Server 2019" ;SearchString="Cumulative Update for Windows Server 2019 for x64-based Systems"    ;PreviewSearchString="Cumulative Update Preview for Windows Server 2019 for x64-based Systems"    ;SSUSearchString="Servicing Stack Update for Windows Server 2019 for x64-based Systemss"}
-$Products+=@{Product="Windows Server 2016" ;SearchString="Cumulative Update for Windows Server 2016 for x64-based Systems"    ;PreviewSearchString="Cumulative Update Preview for Windows Server 2016 for x64-based Systems"    ;SSUSearchString="Servicing Stack Update for Windows Server 2016 for x64-based Systemss"}
-$Products+=@{Product="Windows 10 20H2"     ;SearchString="Cumulative Update for Windows 10 Version 20H2 for x64-based Systems";PreviewSearchString="Cumulative Update Preview for Windows 10 Version 20H2 for x64-based Systems";SSUSearchString="Servicing Stack Update for Windows 10 Version 20H2 for x64-based Systems"}
-$Products+=@{Product="Windows 10 2004"     ;SearchString="Cumulative Update for Windows 10 Version 2004 for x64-based Systems";PreviewSearchString="Cumulative Update Preview for Windows 10 Version 2004 for x64-based Systems";SSUSearchString="Servicing Stack Update for Windows 10 Version 2004 for x64-based Systems"}
-$Products+=@{Product="Windows 10 1909"     ;SearchString="Cumulative Update for Windows 10 Version 1909 for x64-based Systems";PreviewSearchString="Cumulative Update Preview for Windows 10 Version 1909 for x64-based Systems";SSUSearchString="Servicing Stack Update for Windows 10 Version 1909 for x64-based Systems"}
+$Products+=@{Product="Azure Stack HCI 20H2";SearchString="Cumulative Update for Azure Stack HCI, version 20H2"                ;SSUSearchString="Servicing Stack Update for Azure Stack HCI, version 20H2 for x64-based Systems"}
+$Products+=@{Product="Windows Server 2019" ;SearchString="Cumulative Update for Windows Server 2019 for x64-based Systems"    ;SSUSearchString="Servicing Stack Update for Windows Server 2019 for x64-based Systemss"}
+$Products+=@{Product="Windows Server 2016" ;SearchString="Cumulative Update for Windows Server 2016 for x64-based Systems"    ;SSUSearchString="Servicing Stack Update for Windows Server 2016 for x64-based Systemss"}
+$Products+=@{Product="Windows 10 20H2"     ;SearchString="Cumulative Update for Windows 10 Version 20H2 for x64-based Systems";SSUSearchString="Servicing Stack Update for Windows 10 Version 20H2 for x64-based Systems"}
+$Products+=@{Product="Windows 10 2004"     ;SearchString="Cumulative Update for Windows 10 Version 2004 for x64-based Systems";SSUSearchString="Servicing Stack Update for Windows 10 Version 2004 for x64-based Systems"}
+$Products+=@{Product="Windows 10 1909"     ;SearchString="Cumulative Update for Windows 10 Version 1909 for x64-based Systems";SSUSearchString="Servicing Stack Update for Windows 10 Version 1909 for x64-based Systems"}
 
 #grab folder to download to
 $folder=Read-Host -Prompt "Please type path to download. For example `"c:\temp`" (if nothing specified, $PSScriptRoot is used)"
@@ -42,18 +42,15 @@ Foreach($SelectedProduct in $SelectedProducts){
     $item=$Products | Where-Object product -eq $SelectedProduct
     #Download CU
     If ($preview){
-        $update=Get-MSCatalogUpdate -Search $item.previewsearchstring | Select-Object -First 1
-        $DestinationFolder="$folder\$SelectedProduct\$($update.title.Substring(0,7))"
-        New-Item -Path $DestinationFolder -ItemType Directory -ErrorAction Ignore | Out-Null
-        Write-Output "Downloading $($update.title) to $destinationFolder"
-        $update | Save-MSCatalogUpdate -Destination "$DestinationFolder" #-UseBits
+        $update=Get-MSCatalogUpdate -Search $item.searchstring | Select-Object -First 1
     }else{
         $update=Get-MSCatalogUpdate -Search $item.searchstring | Where-Object Title -like "*$($item.SearchString)*" | Select-Object -First 1
-        $DestinationFolder="$folder\$SelectedProduct\$($update.title.Substring(0,7))"
-        New-Item -Path $DestinationFolder -ItemType Directory -ErrorAction Ignore | Out-Null
-        Write-Output "Downloading $($update.title) to $destinationFolder"
-        $update | Save-MSCatalogUpdate -Destination "$DestinationFolder" #-UseBits
     }
+    $DestinationFolder="$folder\$SelectedProduct\$($update.title.Substring(0,7))"
+    New-Item -Path $DestinationFolder -ItemType Directory -ErrorAction Ignore | Out-Null
+    Write-Output "Downloading $($update.title) to $destinationFolder"
+    $update | Save-MSCatalogUpdate -Destination "$DestinationFolder" #-UseBits
+
     #Download SSU
     $update=Get-MSCatalogUpdate -Search $item.SSUSearchString | Select-Object -First 1
     Write-Output "Downloading $($update.title) to $destinationFolder"
