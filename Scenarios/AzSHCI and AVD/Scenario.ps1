33730c9ef4218630d460f32829050405ddde72b8
jaromirk <kaspik@hotmail.com>
2022-03-31 11:57:43 +0200

    comment fix

--- Scenarios/AzSHCI and AVD/Scenario.ps1
+++ Scenarios/AzSHCI and AVD/Scenario.ps1
@@ -166,7 +166,7 @@
     $output=Grant-AzDiskAccess -ResourceGroupName $AVDResourceGroupName -DiskName $ManagedDiskName -Access 'Read' -DurationInSecond 36000 #10 hours
     $SAS=$output.accesssas
     #Start-BitsTransfer -Source $SAS -Destination "\\$ClusterName\ClusterStorage$\$LibraryVolumeName\$SKU.vhd"
-    #download using AzCopy as it's faster than bits transfer. But it cannot be downloaded directly to CSV
+    #download using AzCopy as it's faster than bits transfer
     #https://aka.ms/downloadazcopy-v10-windows\
         # Download the package 
         Start-BitsTransfer -Source "https://aka.ms/downloadazcopy-v10-windows" -Destination "$env:UserProfile\Downloads\AzCopy.zip"
