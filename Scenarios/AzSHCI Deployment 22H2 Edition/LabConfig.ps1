caa00eb21ff6e26d64ad475d937808e06da2b83f
Jaromir Kaspar <kaspik@hotmail.com>
2022-11-23 12:36:13 +0100

    typo fix

--- Scenarios/AzSHCI Deployment 22H2 Edition/LabConfig.ps1
+++ Scenarios/AzSHCI Deployment 22H2 Edition/LabConfig.ps1
@@ -3,7 +3,7 @@ $LabConfig=@{AllowedVLANs="1-10,711-719" ; DomainAdminName='LabAdmin'; AdminPass
 #Azure Stack HCI 21H2
 1..4 | ForEach-Object {$LABConfig.VMs += @{ VMName = "AzSHCI$_" ; Configuration = 'S2D' ; ParentVHD = 'AzSHCI22H2_G2.vhdx' ; HDDNumber = 4 ; HDDSize= 2TB ; MemoryStartupBytes= 1GB; VMProcessorCount=4 ; vTPM=$true}}
 #Or with nested virtualization enabled
-#1..4 | ForEach-Object {$LABonfig.VMs += @{ VMName = "AzSHCI$_" ; ConfiguratioCn = 'S2D' ; ParentVHD = 'AzSHCI22H2_G2.vhdx' ; HDDNumber = 10 ; HDDSize= 10TB ; MemoryStartupBytes= 4GB; VMProcessorCount="4" ; vTPM=$true ; NestedVirt=$true}}
+#1..4 | ForEach-Object {$LABonfig.VMs += @{ VMName = "AzSHCI$_" ; Configuration = 'S2D' ; ParentVHD = 'AzSHCI22H2_G2.vhdx' ; HDDNumber = 4 ; HDDSize= 2TB ; MemoryStartupBytes= 4GB; VMProcessorCount=4 ; vTPM=$true ; NestedVirt=$true}}
 
 #Optional Windows Admin Center in GW mode
 $LabConfig.VMs += @{ VMName = 'WACGW' ; ParentVHD = 'Win2022Core_G2.vhdx'; MGMTNICs=1}
--- Scenarios/AzSHCI Deployment 22H2 Edition/Scenario.ps1
+++ Scenarios/AzSHCI Deployment 22H2 Edition/Scenario.ps1
@@ -426,24 +426,21 @@
         Clear-DnsClientCache
 
     #Configure the host vNIC to use a Vlan.  They can be on the same or different VLans 
-        If ($NumberOfStorageNets -eq 1){
-            Set-VMNetworkAdapterVlan -VMNetworkAdapterName SMB* -VlanId $StorVLAN -Access -ManagementOS -CimSession $Servers
-        }else{
-            #configure Odds and Evens for VLAN1 and VLAN2
-            foreach ($Server in $Servers){
-                $NetAdapters=Get-VMNetworkAdapter -CimSession $server -ManagementOS -Name *SMB* | Sort-Object Name
-                $i=1
-                foreach ($NetAdapter in $NetAdapters){
-                    if (($i % 2) -eq 1){
-                        Set-VMNetworkAdapterVlan -VMNetworkAdapterName $NetAdapter.Name -VlanId $StorVLAN1 -Access -ManagementOS -CimSession $Server
-                        $i++
-                    }else{
-                        Set-VMNetworkAdapterVlan -VMNetworkAdapterName $NetAdapter.Name -VlanId $StorVLAN2 -Access -ManagementOS -CimSession $Server
-                        $i++
-                    }
+        #configure Odds and Evens for VLAN1 and VLAN2
+        foreach ($Server in $Servers){
+            $NetAdapters=Get-VMNetworkAdapter -CimSession $server -ManagementOS -Name *SMB* | Sort-Object Name
+            $i=1
+            foreach ($NetAdapter in $NetAdapters){
+                if (($i % 2) -eq 1){
+                    Set-VMNetworkAdapterVlan -VMNetworkAdapterName $NetAdapter.Name -VlanId $StorVLAN1 -Access -ManagementOS -CimSession $Server
+                    $i++
+                }else{
+                    Set-VMNetworkAdapterVlan -VMNetworkAdapterName $NetAdapter.Name -VlanId $StorVLAN2 -Access -ManagementOS -CimSession $Server
+                    $i++
                 }
             }
         }
+
 
     #Restart each host vNIC adapter so that the Vlan is active.
         Get-NetAdapter -CimSession $Servers -Name "vEthernet (SMB*)" | Restart-NetAdapter
